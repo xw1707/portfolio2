1e151704b391e8d4ba967658259c323a3136f576
xw <1204327445@qq.com>
2020-06-24 21:23:15 +0800

    显示多个gallery

--- portfolio2/views.py
+++ portfolio2/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from gallery.models import Gallery
 
 
 def home(request):
-    return render(request, 'home.html')
+    gallerys = Gallery.objects
+    return render(request, 'home.html', {'gallerys': gallerys})
