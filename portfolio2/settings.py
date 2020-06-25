63ef7cc350f5f0bf337458aba37577568ba83d2f
xw <1204327445@qq.com>
2020-06-25 20:54:36 +0800

    添加静态文件
    修改了gitignore

--- portfolio2/settings.py
+++ portfolio2/settings.py
@@ -119,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'portfolio2/static/'),
+]
+
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 
