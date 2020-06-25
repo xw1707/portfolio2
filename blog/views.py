ae8a33ca617a16a887f627285a33e07eef4739a6
xw <1204327445@qq.com>
2020-06-25 20:17:41 +0800

    标题到正文跳转

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from . models import Blog
 
 # Create your views here.
@@ -7,3 +7,8 @@ from . models import Blog
 def blog_page(request):
     blogs = Blog.objects
     return render(request, 'blog.html', {'blogs': blogs})
+
+
+def blog_text(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog_text.html', {'blog': blog})
